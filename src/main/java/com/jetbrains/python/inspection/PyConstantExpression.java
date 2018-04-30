84090f23c08b46001a9ba4e771f0b95a8430c9ae
mary <mary-fifa3000@mail.ru>
2018-04-30 20:51:27 +0300

    add baseline without negative numbers

--- src/main/java/com/jetbrains/python/inspection/PyConstantExpression.java
+++ src/main/java/com/jetbrains/python/inspection/PyConstantExpression.java
@@ -1,15 +1,22 @@
 package com.jetbrains.python.inspection;
 
+import static com.jetbrains.python.PyTokenTypes.COMPARISON_OPERATIONS;
+import static com.jetbrains.python.PyTokenTypes.*;
+
 import com.intellij.codeInspection.LocalInspectionToolSession;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.python.inspections.PyInspection;
 import com.jetbrains.python.inspections.PyInspectionVisitor;
 import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.resolve.PyResolveContext;
+import com.jetbrains.python.psi.resolve.QualifiedResolveResult;
+import java.math.BigDecimal;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class PyConstantExpression extends PyInspection {
+    public static final String isAlways = "The condition is always ";
 
     @NotNull
     @Override
@@ -34,10 +41,75 @@ public class PyConstantExpression extends PyInspection {
         }
 
         private void processIfPart(@NotNull PyIfPart pyIfPart) {
+            if (checkBool(pyIfPart)) {
+                return;
+            }
+            if (checkIntOpInt(pyIfPart)) {
+                return;
+            }
+
+
+        }
+
+        private boolean checkBool(@NotNull PyIfPart pyIfPart) {
             final PyExpression condition = pyIfPart.getCondition();
             if (condition instanceof PyBoolLiteralExpression) {
-                registerProblem(condition, "The condition is always " + ((PyBoolLiteralExpression) condition).getValue());
+                registerProblem(condition, isAlways + ((PyBoolLiteralExpression) condition).getValue());
+                return true;
             }
+            return false;
         }
+
+        private boolean checkIntOpInt(@NotNull PyIfPart pyIfPart) {
+            final PyExpression condition = pyIfPart.getCondition();
+
+            if (!(condition instanceof PyBinaryExpression)) {
+                return false;
+            }
+            PyBinaryExpression binaryExpression = (PyBinaryExpression) condition;
+            PyElementType operator = (binaryExpression).getOperator();
+
+            PyExpression left = binaryExpression.getLeftExpression();
+            PyExpression right = binaryExpression.getRightExpression();
+            if ((RELATIONAL_OPERATIONS.contains(operator) || EQUALITY_OPERATIONS.contains(operator))
+                && left instanceof PyNumericLiteralExpression
+                && right instanceof PyNumericLiteralExpression) {
+                BigDecimal leftValue = ((PyNumericLiteralExpression) left).getBigDecimalValue();
+                BigDecimal rightValue = ((PyNumericLiteralExpression) right).getBigDecimalValue();
+                registerProblem(condition, isAlways + boolValue(calculateStatement(leftValue, rightValue, operator)));
+                return true;
+            }
+            return false;
+        }
+
+        private String boolValue(boolean b) {
+            return b ? "true" : "false";
+        }
+
+        private boolean calculateStatement(BigDecimal left, BigDecimal right, PyElementType operator) {
+            int result = left.compareTo(right);
+            if (operator == LT) {
+                return result < 0;
+            }
+            if (operator == GT) {
+                return result > 0;
+            }
+            if (operator == EQEQ) {
+                return result == 0;
+            }
+            if (operator == GE) {
+                return result >= 0;
+            }
+            if (operator == LE) {
+                return result <= 0;
+            }
+            if (operator == NE || operator == NE_OLD) {
+                return result != 0;
+            }
+
+            //Unreachable
+            return false;
+        }
+
     }
 }
---
+++ tests/test_int_op_int.py
@@ -0,0 +1,14 @@
+if 5 == 5:
+    pass
+
+if 3 > 10:
+    pass
+
+if 5 > -1:
+    pass
+
+if 3 == 2:
+    pass
+
+if 4 in 4:
+    pass
